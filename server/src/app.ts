bbcd5ccb3351361aa46563d5788196238f7ef65b
K-ash-ish <kashishsondhiya@gmail.com>
2024-07-23 20:41:49 +0530

    c-c messages

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,30 +1,46 @@
-import http from "http";
+import { strict } from "assert";
+import http, { IncomingMessage } from "http";
 import { parse } from "url";
-import { WebSocketServer } from "ws";
+import { Data, WebSocketServer, WebSocket } from "ws";
 
 const server = http.createServer();
 
 const wss = new WebSocketServer({ server });
 
+type client = {
+  ws: WebSocket;
+  token: string;
+};
+
+const CLIENTS: client[] = [];
+
 //
-wss.on("connection", (ws, req) => {
-    console.log("Client connected");
-    const id = req.headers["sec-websocket-key"];
-    console.log("ID: ", id);
-    console.log("Req headers: ", req.headers);
-    const {
-        query: { token },
-    } = parse(req.url, true);
-    console.log("TOKEN: ", token);
+wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
+  const {
+    query: { token },
+  } = parse(req.url || "", true);
 
-    //received message
-    ws.on("message", function message(data) {
-        wss.clients.forEach(function each(client) {});
-        console.log("Recieved: ", data);
-    });
+  console.log("TOKEN: ", token);
+  const client: client = {
+    ws,
+    token: token as string,
+  };
+  CLIENTS.push(client);
 
-    //send to client
-    ws.send("Something");
+  //received message
+  ws.on("message", function message(data: Data) {
+    console.log(data.toString());
+    const receivedData = JSON.parse(data.toString());
+    const client = CLIENTS.filter((client) => client.token === receivedData.to);
+    client[0].ws.send(receivedData.message);
+
+    // wss.clients.forEach(function each(client) {
+    //   console.log(client);
+    // });
+  });
+
+  //send to client
+  ws.send("From Server");
 });
 
 server.listen(3000);
