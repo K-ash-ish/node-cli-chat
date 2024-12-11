e9d49dd96aec53a548f7a134519b0ea072c90ba5
K-ash-ish <kashishsondhiya@gmail.com>
2024-12-11 21:24:05 +0530

    github action for CI/CD

---
+++ client/src/constant.ts
@@ -0,0 +1,4 @@
+// export const URL = "http://localhost:4000/";
+export const URL = "https://node-cli-chat.onrender.com/";
+// export const WS_URL = "ws://localhost:4000/";
+export const WS_URL = "wss://node-cli-chat.onrender.com/";
